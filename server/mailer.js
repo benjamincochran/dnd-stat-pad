e53b54fe79e6a2bdb5224b2973078bc8452dfdb8
benjamincochran <benjamin.cochran@gmail.com>
2018-01-25 22:35:33 -0500

    remove comment

--- server/mailer.js
+++ server/mailer.js
@@ -1,6 +1,5 @@
 import nodemailer from 'nodemailer'
 
-// create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
